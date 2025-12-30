5aa5dd4014d7cbcc5a4ae5dc6781f5735eb5066a
Quang Huy <167593677+Huy1401-codes@users.noreply.github.com>
2025-12-30 15:23:03 +0700

    Update avatar author

--- VBNet/MyLibrary.BLL/DTOs/Authors/AuthorDetailDto.vb
+++ VBNet/MyLibrary.BLL/DTOs/Authors/AuthorDetailDto.vb
@@ -1,5 +1,6 @@
 ﻿Public Class AuthorDetailDto
     Public Property AuthorId As Integer
     Public Property AuthorName As String
+    Public Property Avatar As String
     Public Property Books As PagedResult(Of AuthorBookDto)
 End Class
